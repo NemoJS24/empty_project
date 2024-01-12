d084cb5b0cc73554303bf098076b19f917411939
Prasad Gawade <prasadgawade249@gmail.com>
2024-01-12 13:00:02 +0530

    Call Changes

--- src/views/CRM/Call/AddCall.js
+++ src/views/CRM/Call/AddCall.js
@@ -1,16 +1,84 @@
 import React, { useState } from 'react'
 import { Card, CardBody, Container, Row, Col, Input } from "reactstrap"
 import Select from "react-select"
+import { validForm } from '../../Validator'
+// import { validForm } from "../Validator"
 
 const AddCall = () => {
-   const [formData, setFormData] = useState({})
 
+   const valueToCheck = [
+      {
+         name: 'customer_name',
+         message: 'Enter Customer Name',
+         type: 'string',
+         id: 'customer_name'
+      },
+      {
+         name: 'call_status',
+         message: 'Select Call Status',
+         type: 'string',
+         id: 'call_status'
+      },
+      {
+         name: 'call_purpose',
+         message: 'Select Call Purpose',
+         type: 'string',
+         id: 'call_purpos'
+      },
+      {
+         name: 'lead_status',
+         message: 'Select Lead Status',
+         type: 'string',
+         id: 'lead_status'
+      },
+      {
+         name: 'interested',
+         message: 'Select Interested',
+         type: 'string',
+         id: 'interested'
+      }
+   ]
+
+   const [formData, setFormData] = useState({ schedule_call: false })
+   const [data, setData] = useState({
+      customer_name: "",
+      call_status: "",
+      call_purpose: "",
+      lead_status: "",
+      interested: ""
+   })
+
+   const inputChangeHandler = (e) => {
+      setData({ ...data, [e.target.name]: e.target.value })
+   }
+
+   const handleSubmitSection = (e, action) => {
+      e.preventDefault()
+
+      const checkForm = validForm(valueToCheck, data)
+      console.log(checkForm)
+
+      // valueToCheck.forEach(validation => {
+      //    const errorMessageElement = document.getElementById(`${validation.id}_error`)
+      //    errorMessageElement.innerText = checkForm[validation.name] ? '' : validation.message
+      // })
+
+      if (checkForm.isValid) {
+         console.log("Form is valid")
+
+         if (action === 'SAVE') {
+            // Save
+         } else if (action === 'SAVE & CLOSE') {
+            // Save and close
+         }
+      }
+   }
    const handleInputChange = (e, type) => {
       if (type === undefined) {
-         const { name, value } = e.target
+         const { name, value, type } = e.target
          setFormData(prev => ({
             ...prev,
-            [name]: value
+            [name]: type === 'checkbox' ? !prev[name] : value
          }))
       } else {
          setFormData(prev => ({
@@ -70,152 +138,170 @@ const AddCall = () => {
                               <h4 className="mb-0">Call Details</h4>
                            </Col>
                            <Col md={6} className="mt-2">
-                              <label htmlFor="customer-name">
-                                 Customer Name
-                              </label>
+                              <label htmlFor="customer-name">Customer Name</label>
                               <input type='text' id='customer-name' name='customer_name' className="form-control"
-                                 value={formData?.customer_name}
-                              // onChange={handleInputChange} 
-                              // disabled={id || viewPage}
+                                 value={data?.customer_name}
+                                 onChange={(e) => {
+                                    inputChangeHandler(e)
+                                 }}
                               />
+                              <p id="customer_name_val" className="text-danger m-0 p-0 vaildMessage"></p>
                            </Col>
                            <Col md={6} className="mt-2">
-                              <label htmlFor="call-status" className="" style={{ margin: '0px' }}>
-                                 Call Status
-                              </label>
+                              <label htmlFor="call_status">Call Status</label>
                               <Select
                                  placeholder='Call Status'
-                                 id="call-status"
-                                 // isDisabled={viewPage}
+                                 id="call_status"
                                  options={callStatusOptions}
                                  closeMenuOnSelect={true}
-                              // value={callStatusOptions?.filter(option => option.value === formData?.vehicle_type)}
-                              // onChange={e => handleInputChange(e, 'vehicle_type')}
+                                 onChange={(e) => {
+                                    inputChangeHandler({ target: { name: 'call_status', value: e?.value } })
+                                 }}
                               />
+                              <p id="call_status_val" className="text-danger m-0 p-0 vaildMessage"></p>
                            </Col>
                            <Col md={6} className="mt-2">
-                              <label htmlFor="call-purpos" className="" style={{ margin: '0px' }}>
-                                 Call Purpose
-                              </label>
+                              <label htmlFor="call_purpos">Call Purpose</label>
                               <Select
                                  placeholder='Call Purpose'
-                                 id="call-purpos"
-                                 // isDisabled={viewPage}
+                                 id="call_purpos"
                                  options={callPurposeOptions}
                                  closeMenuOnSelect={true}
-                              // value={callPurposeOptions?.filter(option => option.value === formData?.vehicle_type)}
-                              // onChange={e => handleInputChange(e, 'vehicle_type')}
+                                 onChange={(e) => {
+                                    inputChangeHandler({ target: { name: 'call_purpose', value: e?.value } })
+                                 }}
                               />
+                              <p id="call_purpos_val" className="text-danger m-0 p-0 vaildMessage"></p>
                            </Col>
                            <Col md={6} className="mt-2">
-                              <label htmlFor="lead-status" className="" style={{ margin: '0px' }}>
+                              <label htmlFor="lead_status" className="" style={{ margin: '0px' }}>
                                  Lead Status
                               </label>
                               <Select
                                  placeholder='Lead Status'
-                                 id="lead-status"
+                                 id="lead_status"
                                  // isDisabled={viewPage}
                                  options={leadStatusOptions}
                                  closeMenuOnSelect={true}
+                                 onChange={(e) => {
+                                    inputChangeHandler({ target: { name: 'lead_status', value: e?.value } })
+                                 }}
                               // value={leadStatusOptions?.filter(option => option.value === formData?.vehicle_type)}
                               // onChange={e => handleInputChange(e, 'vehicle_type')}
                               />
+                              <p id="lead_status_val" className="text-danger m-0 p-0 vaildMessage"></p>
                            </Col>
                            <Col md={12} className="mt-2">
                               <label htmlFor="notes-label" className="" style={{ margin: '0px' }}>
                                  Notes
                               </label>
                               {/* <div className="form-floating"> */}
-                                 <textarea className="form-control" placeholder="Leave a note here" id="notes-label" style={{ minHeight: '90px' }}></textarea>
-                                 {/* <label htmlFor="notes-label">Notes</label> */}
+                              <textarea className="form-control" placeholder="Leave a note here" id="notes-label" style={{ minHeight: '90px' }}></textarea>
+                              {/* <label htmlFor="notes-label">Notes</label> */}
                               {/* </div> */}
                            </Col>
                            <Col md={6} className="mt-2">
-                              <label htmlFor="interested-label" className="" style={{ margin: '0px' }}>
+                              <label htmlFor="interested" className="" style={{ margin: '0px' }}>
                                  Interested
                               </label>
                               <Select
                                  placeholder='Interested'
-                                 id="interested-label"
+                                 id="interested"
                                  // isDisabled={viewPage}
                                  options={linterestedOptions}
                                  closeMenuOnSelect={true}
+                                 onChange={(e) => {
+                                    inputChangeHandler({ target: { name: 'interested', value: e?.value } })
+                                 }}
                               // value={linterestedOptions?.filter(option => option.value === formData?.vehicle_type)}
                               // onChange={e => handleInputChange(e, 'vehicle_type')}
                               />
+                              <p id="interested_val" className="text-danger m-0 p-0 vaildMessage"></p>
                            </Col>
-                           <Col md={12} className="mt-2">
-                              <div>
-                                 {/* <span className="form-check form-check-primary"> */}
-                                 <input
-                                    type="checkbox"
-                                    id="schedule-next-call"
-                                    className="form-check-input"
-                                    name="schedule_call"
-                                    checked={formData?.schedule_call}
-                                    onChange={handleInputChange}
-                                 />
-                                 <label htmlFor="schedule-next-call" style={{ marginLeft: '10px' }}>Schedule Next Call</label>
-                                 {/* </span> */}
-                              </div>
-                           </Col>
+                           <Row>
+                              <Col md={12} className="mt-2">
+                                 <div className="d-flex justify-content-start align-items-center gap-1">
+                                    <input
+                                       type="checkbox"
+                                       id="schedule_next_call"
+                                       className="form-check-input cursor-pointer"
+                                       name="schedule_call"
+                                       checked={formData.schedule_call}
+                                       onChange={handleInputChange}
+                                    />
+                                    <label htmlFor="schedule_next_call">Schedule Next Call</label>
+                                 </div>
+                              </Col>
 
-                           {formData?.schedule_call && <>
-                              <Col md={6} className="mt-2">
-                                 <label htmlFor="vehicle-delivery-date">
-                                    Date:
-                                 </label>
-                                 <input placeholder="Date" type='date' id='vehicle-delivery-date' name='product.delivery_date' className="form-control"
-                                    value={formData?.delivery_date}
-                                    onChange={handleInputChange}
-                                 />
+                              {formData.schedule_call && (
+                                 <>
+                                    {/* ... (previous code) */}
+                                    <Col md={6} className="mt-2">
+                                       <label htmlFor="vehicle-delivery-date">Date:</label>
+                                       <input
+                                          placeholder="Date"
+                                          type="date"
+                                          id="vehicle-delivery-date"
+                                          name="delivery_date"
+                                          className="form-control"
+                                          value={formData.delivery_date}
+                                          onChange={handleInputChange}
+                                       />
+                                    </Col>
+                                    <Col md={6} className="mt-2">
+                                       <label htmlFor="vehicle-delivery-date">Time:</label>
+                                       <input
+                                          type="time"
+                                          id="appt"
+                                          name="time"
+                                          className="form-control"
+                                          value={formData.time}
+                                          onChange={handleInputChange}
+                                       />
+                                    </Col>
+                                 </>
+                              )}
+                           </Row>
+
+                           <Row>
+                              <Col md={12} className="mt-2">
+                                 <h4 className="mb-0">Send</h4>
                               </Col>
-                              <Col md={6} className="mt-2">
-                                 <label htmlFor="vehicle-delivery-date">
-                                    Time:
-                                 </label>
-                                 <input type="time" id="appt" name="appt" className='form-control'
-                                    value={formData?.time}
-                                    onChange={handleInputChange}
-                                 />
+                              <Col md={12} className="mt-1">
+                                 <div className="d-flex justify-content-start align-items-center gap-1">
+                                    <input
+                                       type="checkbox"
+                                       id="send_email"
+                                       className="form-check-input m-0 p-0 cursor-pointer"
+                                       name="email"
+                                       checked={formData?.email}
+                                       onChange={handleInputChange}
+                                    />
+                                    <label htmlFor="send_email" >Send Email</label>
+                                 </div>
                               </Col>
-                           </>}
-                           <Col md={12} className="mt-2">
-                              <h4 className="mb-0">Send</h4>
-                           </Col>
-                           <Col md={12} className="mt-1">
-                              <div>
-                                 <input
-                                    type="checkbox"
-                                    id="schedule-next-call"
-                                    className="form-check-input"
-                                    name="email"
-                                    checked={formData?.email}
-                                    onChange={handleInputChange}
-                                 />
-                                 <label htmlFor="schedule-next-call" style={{ marginLeft: '10px' }}>Send Email</label>
-                              </div>
-                           </Col><Col md={12} className="mt-1">
-                              <div>
-                                 <input
-                                    type="checkbox"
-                                    id="schedule-next-call"
-                                    className="form-check-input"
-                                    name="sms"
-                                    checked={formData?.sms}
-                                    onChange={handleInputChange}
-                                 />
-                                 <label htmlFor="schedule-next-call" style={{ marginLeft: '10px' }}>Send SMS</label>
-                              </div>
-                           </Col>
+                              <Col md={12} className="mt-1">
+                                 <div className="d-flex justify-content-start align-items-center gap-1">
+                                    <input
+                                       type="checkbox"
+                                       id="send_sms"
+                                       className="form-check-input cursor-pointer"
+                                       name="sms"
+                                       checked={formData?.sms}
+                                       onChange={handleInputChange}
+                                    />
+                                    <label htmlFor="send_sms">Send SMS</label>
+                                 </div>
+                              </Col>
+                           </Row>
                         </Row>
-                        <div className='w-100 d-flex justify-content-between mt-2'>
+                        <div className='w-100 d-flex justify-content-between mt-3'>
                            <div>
                               <button className="btn btn-primary" type="button" onClick={() => navigate(-1)} >Back</button>
                            </div>
                            {/* {!viewPage &&  */}
-                           <div>
-                              <button className="btn btn-primary ms-2" type="button" onClick={e => handleSubmitSection(e, 'SAVE')} >Save</button>
+                           <div className='d-flex justify-content-center align-items-center gap-2'>
+                              <button className="btn btn-primary ms-2" type="button" onClick={e => handleSubmitSection(e, 'SAVE')}>Save</button>
                               <button className="btn btn-primary" type="button" onClick={e => handleSubmitSection(e, 'SAVE & CLOSE')} >Save & Close</button>
                            </div>
                            {/* } */}
